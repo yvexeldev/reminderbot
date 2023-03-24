559ffa0acd6085528239881d19e9eed2cb3a3006
Abdusalomov Azam <97389595+ritnyxdev@users.noreply.github.com>
2023-03-24 16:35:34 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Telegraf } from "telegraf";
 
-const bot = new Telegraf("<BOT_TOKEN>");
-const CHANEL_ID = "<CHANNEL_ID>";
+const bot = new Telegraf("6042837277:AAGeNy0JBAP5zOuWSS9ZpbdqTvWbvSWB98M");
+const CHANEL_ID = "-1001667489032";
 
 const messages = [
     "Say - Subhanallohil Aziym 🌙",
@@ -26,43 +26,43 @@ setInterval(() => {
     if (currentHour == 0 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[0]);
     }
-    if (currentHour == 1 && currentMinute == 1 && currentSeconds == 1) {
+    if (currentHour == 13 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[1]);
     }
-    if (currentHour == 2 && currentMinute == 2 && currentSeconds == 2) {
+    if (currentHour == 14 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[2]);
     }
-    if (currentHour == 3 && currentMinute == 3 && currentSeconds == 3) {
+    if (currentHour == 15 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[3]);
     }
-    if (currentHour == 4 && currentMinute == 4 && currentSeconds == 4) {
+    if (currentHour == 16 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[4]);
     }
-    if (currentHour == 5 && currentMinute == 5 && currentSeconds == 5) {
+    if (currentHour == 17 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[5]);
     }
-    if (currentHour == 6 && currentMinute == 6 && currentSeconds == 6) {
+    if (currentHour == 6 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[6]);
     }
-    if (currentHour == 7 && currentMinute == 7 && currentSeconds == 7) {
+    if (currentHour == 7 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[7]);
     }
-    if (currentHour == 8 && currentMinute == 8 && currentSeconds == 8) {
+    if (currentHour == 8 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[8]);
     }
-    if (currentHour == 9 && currentMinute == 9 && currentSeconds == 9) {
+    if (currentHour == 9 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[9]);
     }
 
-    if (currentHour == 10 && currentMinute == 10 && currentSeconds == 10) {
+    if (currentHour == 10 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[10]);
     }
 
-    if (currentHour == 11 && currentMinute == 11 && currentSeconds == 11) {
+    if (currentHour == 11 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[11]);
     }
 
-    if (currentHour == 12 && currentMinute == 12 && currentSeconds == 12) {
+    if (currentHour == 12 && currentMinute == 0 && currentSeconds == 0) {
         bot.telegram.sendMessage(CHANEL_ID, messages[12]);
     } else {
         console.log("EE");
